398784aa41eeb51c2b45b9e5a1bedc7bf624b12c
Tulai Dmitry <dtylai@icloud.com>
2021-03-31 14:52:04 +0300

    Initial Commit

---
+++ NewsFeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeedApp
+//
+//  Created by Tulai Dima on 31.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
